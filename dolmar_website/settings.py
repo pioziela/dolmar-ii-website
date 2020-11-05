c64760d4f023cd36adf604ffe413cdc299ce8c31
zielapio <piotr.zielinski.cv@gmail.com>
2020-11-05 03:06:28 +0100

    allowed hosts

--- dolmar_website/settings.py
+++ dolmar_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["192.109.241.69"]
 
 
 # Application definition
